9322b76914ce0ba30ee5c1a68e6d7799417d3085
Kevin Karma <kevinkarma55@gmail.com>
2019-10-04 17:08:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name,event){
+  var result = []
+  for(var i = 0; i < name.length; i ++){
+    result.push("Thank you, " + name[i] + ", for the wonderful " + event + " gift!"
+   );
+  }
+  return result;
+}
+
+function countdown(number){
+  while(0 <= number){
+    console.log(number);
+    number = number - 1;
+  }
+}
