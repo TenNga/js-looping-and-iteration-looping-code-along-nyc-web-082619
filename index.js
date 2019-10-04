d567f7d8f6f3203d66d3a149613b163a5c0d6e4f
Kevin Karma <kevinkarma55@gmail.com>
2019-10-04 16:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(name,event){
   var result = []
   for(var i = 0; i < name.length; i ++){
-    result.push("Thank you " + name[i] + " for coming to " + event
+    result.push("Thank you, " + name[i] + ", for the wonderful " + event + " gift!"
    );
    return result;
   }
